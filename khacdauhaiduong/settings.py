ba06a7e9a24ba65e47f82be41a2222f9c8e48741
thanhvu9284 <thanhvu9284@gmail.com>
2021-10-07 16:35:50 +0700

    khacdauhaiduong

--- khacdauhaiduong/settings.py
+++ khacdauhaiduong/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-nez#l&)o4^qvcfv^1njryzxx@clha30xh=wzu8++^jr66es*_q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS =  ['127.0.0.1','https://khacdauhaiduong.herokuapp.com/']
+#ALLOWED_HOSTS =  ['127.0.0.1','https://khacdauhaiduong.herokuapp.com/']
 
 CKEDITOR_UPLOAD_PATH = 'uploads/'
 
